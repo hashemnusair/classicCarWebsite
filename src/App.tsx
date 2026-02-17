bddc960bbd2dcb6c7fdc7f4af69eda2c5aed0e71
Hashem Nusair <nusairhashem04@gmail.com>
2026-02-17 22:36:58 +0300

    chore: add vercel speed insights tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import { LanguageProvider } from './context/LanguageContext'
 import Layout from './components/layout/Layout'
 import Home from './pages/Home'
@@ -8,10 +9,18 @@ import About from './pages/About'
 import Branches from './pages/Branches'
 import Contact from './pages/Contact'
 
+function SpeedInsightsTracker() {
+  const location = useLocation()
+  const route = `${location.pathname}${location.search}${location.hash}`
+
+  return <SpeedInsights route={route} />
+}
+
 export default function App() {
   return (
     <BrowserRouter>
       <LanguageProvider>
+        <SpeedInsightsTracker />
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
